ba9b096512a5ae677e24353adf5d119fd7bee698
sadasivuni durga <durgasadasivuni23@gmail.com>
2022-09-27 22:48:36 +0530

    Add files via upload

---
+++ employees.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(250) NOT NULL,
+  `Email` varchar(250) NOT NULL,
+  `contact` varchar(250) NOT NULL,
+  `PASSWORD` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
